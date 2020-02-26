e177e90dbc8008f62ed8e92cc7d5dde28a2f1c6e
Matthias Holzer <mh@nerdmania.de>
2020-02-26 23:14:14 +0100

    add start script, modify main package, fix build script, remove unneded cors, add implementation for filteredimage

--- course-02/project/image-filter-starter-code/src/controller/v0/filter/route/filter.router.ts
+++ course-02/project/image-filter-starter-code/src/controller/v0/filter/route/filter.router.ts
@@ -1,34 +1,25 @@
 import { Router, Request, Response } from "express";
+import { filterImageFromURL, deleteLocalFiles } from "../../../../util/util";
 
 const router: Router = Router();
 
 router.get("/filteredimage", async (req: Request, res: Response) => {
-  const { image_url } = req.query.image_url;
-
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
-
-  res.status(201).send({});
+  const imageUrl: string = req.query.image_url;
+  if (imageUrl) {
+    filterImageFromURL(imageUrl).then(response => {
+      res.sendFile(response);
+      res.on("finish", function() {
+        deleteLocalFiles([response]);
+      });
+    });
+  } else {
+    res.status(404).send("Please provide an image url as a query parameter");
+  }
 });
 
 // Root URI call
-app.get("/", async (req: Request, res: Response) => {
+router.get("/", async (req: Request, res: Response) => {
   res.send("/api/v0/");
 });
 
-export const FeedRouter: Router = router;
+export const FilterRouter: Router = router;
--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { filterImageFromURL, deleteLocalFiles } from "./util/util";
+
 import { IndexRouter } from "./controller/v0/index.router";
 
 (async () => {
@@ -13,23 +13,12 @@ import { IndexRouter } from "./controller/v0/index.router";
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // Root Endpoint
-  // Displays a simple message to the user
-  app.get("/", async (req, res) => {
-    res.send("try GET /filteredimage?image_url={{}}");
-  });
-
-  //CORS Should be restricted
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    next();
-  });
-
   app.use("/", IndexRouter);
+
+  // Root URI call
+  app.get("/", async (req, res) => {
+    res.send("/");
+  });
 
   // Start the Server
   app.listen(port, () => {
