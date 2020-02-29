85fefcd77cb335055e4b77af8941d4aa22d7e464
Matthias Holzer <mh@nerdmania.de>
2020-02-29 12:07:20 +0100

    Add new feature to filter images in HQ

--- course-02/project/image-filter-starter-code/src/controller/v0/filteredimage/route/filteredimage.router.ts
+++ course-02/project/image-filter-starter-code/src/controller/v0/filteredimage/route/filteredimage.router.ts
@@ -1,5 +1,9 @@
 import { Router, Request, Response } from "express";
-import { filterImageFromURL, deleteLocalFiles } from "../../../../util/util";
+import {
+  filterImageFromURL,
+  deleteLocalFiles,
+  filterImageFromURLHQ
+} from "../../../../util/util";
 
 const router: Router = Router();
 
@@ -7,6 +11,20 @@ router.get("/filteredimage", async (req: Request, res: Response) => {
   const imageUrl: string = req.query.image_url;
   if (imageUrl) {
     filterImageFromURL(imageUrl).then(response => {
+      res.sendFile(response);
+      res.on("finish", function() {
+        deleteLocalFiles([response]);
+      });
+    });
+  } else {
+    res.status(404).send("Please provide an image url as a query parameter");
+  }
+});
+
+router.get("/filteredimageHQ", async (req: Request, res: Response) => {
+  const imageUrl: string = req.query.image_url;
+  if (imageUrl) {
+    filterImageFromURLHQ(imageUrl).then(response => {
       res.sendFile(response);
       res.on("finish", function() {
         deleteLocalFiles([response]);
